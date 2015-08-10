82913e1deded329ede7fc0501280ad7900513c71
Michael Neylon <michael.t.neylon@gmail.com>
2015-08-10 10:24:24 -0500

    Models case fix, home page updates
    models queries table name case sensitive on mysql running in linux -
    fixed case. Remove base_url from config, allow Jenkins to configure
    that. Improved home page.

--- HGTR/application/models/Kit_model.php
+++ HGTR/application/models/Kit_model.php
@@ -25,73 +25,73 @@ class Kit_model extends CI_Model {
 	
 	function get_genes()
 	{
-		$sql = 'SELECT ENTREZ_GENE_ID,SYMBOL FROM HUMAN_GENE';
+		$sql = 'SELECT ENTREZ_GENE_ID,SYMBOL FROM human_gene';
 		$query = $this->db->query($sql);
 		return $query->result();
 	}		
 	function get_platforms()
 	{
-		$sql = 'SELECT PLATFORM FROM PLATFORM';
+		$sql = 'SELECT PLATFORM FROM platform';
 		$query = $this->db->query($sql);
 		return $query->result();
 	}
 	function get_category()
 	{
-		$sql = 'SELECT MAJOR_METHOD,METHOD,INPUT_VALUE as value FROM CATEGORY';
+		$sql = 'SELECT MAJOR_METHOD,METHOD,INPUT_VALUE as value FROM category';
 		$query = $this->db->query($sql);
 		return $query->result();
 	}
 	function get_method() 
 	{
-		$sql = 'SELECT PRIMARY_METHODOLOGY as method FROM METHOD';
+		$sql = 'SELECT PRIMARY_METHODOLOGY as method FROM method';
 		$query = $this->db->query($sql);
 		return $query->result();
 	}
 	function get_instruments()
 	{
-		$sql = 'SELECT INSTRUMENT FROM INSTRUMENT';
+		$sql = 'SELECT INSTRUMENT FROM instrument';
 		$query = $this->db->query($sql);
 		return $query->result();
 	}
 	function get_purpose()
 	{
-		$sql = 'SELECT PURPOSE FROM PURPOSE';
+		$sql = 'SELECT PURPOSE FROM purpose';
 		$query = $this->db->query($sql);
 		return $query->result();
 	}
 	function get_diseased()
 	{
-		$sql = 'SELECT NAME FROM DISEASE';
+		$sql = 'SELECT NAME FROM disease';
 		$query = $this->db->query($sql);
 		return $query->result();
 	}
 	function get_imgt_hla_versions()
 	{
-		$sql = 'SELECT VERSION FROM IMGT_HLA_META';
+		$sql = 'SELECT VERSION FROM imgt_hla_meta';
 		$query = $this->db->query($sql);
 		return $query->result();
 	}
 	function get_gene_id($gene_symbol)
 	{
-		$sql = 'SELECT ENTREZ_GENE_ID FROM HUMAN_GENE WHERE SYMBOl = ?';
+		$sql = 'SELECT ENTREZ_GENE_ID FROM human_gene WHERE SYMBOl = ?';
 		$query = $this->db->query($sql, array($gene_symbol));
 		return $query->row_array();
 	}
 	function check_instruments($instrument)
 	{
-		$sql = 'SELECT * FROM INSTRUMENT WHERE INSTRUMENT=?';
+		$sql = 'SELECT * FROM instrument WHERE INSTRUMENT=?';
 		$query = $this->db->query($sql, array($instrument));
 		return $query->row_array();
 	}
 	function check_method($primary_method)
 	{
-		$sql = 'SELECT * FROM METHOD WHERE PRIMARY_METHODOLOGY=?';
+		$sql = 'SELECT * FROM method WHERE PRIMARY_METHODOLOGY=?';
 		$query = $this->db->query($sql, array($primary_method));
 		return $query->row_array();
 	}
 	function check_platform($platform)
 	{
-		$sql = 'SELECT * FROM PLATFORM WHERE PLATFORM = ?';
+		$sql = 'SELECT * FROM platform WHERE PLATFORM = ?';
 		$query = $this->db->query($sql, array($platform));
 		return $query->row_array();
 	}
